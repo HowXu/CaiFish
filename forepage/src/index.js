7867a07b488c3ff5850e6f26e4a52a0d5389522f
HowXu <howxu366@outlook.com>
2024-07-04 12:19:08 +0800

    finsh web page

--- forepage/src/index.js
+++ forepage/src/index.js
@@ -1,5 +1,4 @@
 
-
 function onScroll (){
 
     let top_ht = document.documentElement.scrollTop;
@@ -7,16 +6,22 @@ function onScroll (){
     
     let title = document.getElementById("title");
     let dec = document.getElementById("dec");
+    let video = document.getElementById("video");
 
 	if(top_ht >= scroll_ht / 3){
         let transparent_hdrd = ((top_ht - scroll_ht /3 ) / (scroll_ht / 3));
+
+        //把这三个货放在一个div不就得了？
 		title.style.display="inherit";
         dec.style.display="inherit";
+        video.style.display="inherit";
 
         title.style.opacity = transparent_hdrd
         dec.style.opacity = transparent_hdrd
+        video.style.opacity = transparent_hdrd
 	}else{
-		title.style.display="none";
-        dec.style.display="none";
+		title.style.opacity = 0
+        dec.style.opacity = 0
+        video.style.opacity = 0
 	}
 }
